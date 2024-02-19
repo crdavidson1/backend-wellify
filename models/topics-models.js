93a69f6debc60709817f7528b114714bcff3ee80
crdavidson1 <callum.r.davidson@hotmail.com>
2024-02-19 16:50:07 +0000

    updates based on comments

--- models/topics-models.js
+++ models/topics-models.js
@@ -9,6 +9,9 @@ exports.selectTopics = () => {
 exports.selectArticleById = (article_id) => {
     return db.query('SELECT * FROM articles WHERE article_id = $1;', [article_id])
     .then((result) => {
+        if(result.rows.length === 0) {
+            return Promise.reject({status: 404, msg: 'article does not exist'})
+        }
         return result.rows[0]
     })
 }
