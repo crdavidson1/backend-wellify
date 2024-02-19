403ac55a583edf16e91fc65dda4d46fd2b5b0eea
crdavidson1 <callum.r.davidson@hotmail.com>
2024-02-19 14:45:28 +0000

    completes task 3

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
-const {getTopics} = require('./controllers/topics-controllers')
+const {getTopics, getEndpoints} = require('./controllers/topics-controllers')
 
 app.get('/api/topics', getTopics)
+
+app.get('/api', getEndpoints)
+
+app.get('*', function(req, res){
+  res.status(404).send({msg:'Not found'});
+});
+
+app.use((error, request, response, next) => {
+  if (error.status && error.msg) {
+      response.status(error.status).send({msg: error.msg})
+  } else {
+      next()
+  }
+})
   
 module.exports = app;
