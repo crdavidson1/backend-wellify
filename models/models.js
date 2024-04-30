a559fc15998922e28da86004d27719cd596242fc
crdavidson1 <callum.r.davidson@hotmail.com>
2024-04-30 16:40:31 +0100

    fixes endpoint

--- models/models.js
+++ models/models.js
@@ -2,5 +2,7 @@ const db = require('../db/connection.js')
 const format = require('pg-format')
 
 exports.selectCustomers = () => {
-    return db.query('SELECT * FROM wellify.customers')
+    return db.query('SELECT * FROM wellify.customers;').then((result) => {
+        return result.rows
+    })
 }
